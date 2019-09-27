5df9c66174f8b54b49f3ae2474fbfe8154b232bd
Maks <maks112v@gmail.com>
2019-09-27 12:26:02 -0400

    Added measurement id

--- src/firebase.js
+++ src/firebase.js
@@ -11,6 +11,7 @@ const firebaseConfig = {
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
